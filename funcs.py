c86a2a4e252fa7ea63e5b0cff1ed63ff93a26b40
Dmitry Dronov <cskafan1911@gmail.com>
2023-07-30 10:27:02 +0300

    change subtracts function

--- funcs.py
+++ funcs.py
@@ -11,6 +11,6 @@ def subtracts_two_numbers(number_1, number_2):
     """
     Функция вычитает два числа
     """
-    difference = number_1 + number_2
+    difference = number_1 - number_2
 
     return difference
