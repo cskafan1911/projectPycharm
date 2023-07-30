cdd64ff2469cbeddba65205e12f7964b4d01583e
Dmitry Dronov <cskafan1911@gmail.com>
2023-07-30 10:17:10 +0300

    add new function

--- funcs.py
+++ funcs.py
@@ -5,3 +5,12 @@ def adds_two_numbers(number_1, number_2):
     summ_ = number_1 + number_2
 
     return summ_
+
+
+def subtracts_two_numbers(number_1, number_2):
+    """
+    Функция вычитает два числа
+    """
+    difference = number_1 + number_2
+
+    return difference
